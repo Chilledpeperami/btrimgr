1c748c58c0b22d8d5360f77d70a1957807a143e1
Chilledpeperami <chilledpeperami@localhost.localdomain>
2016-05-05 12:56:29 +0200

    Layed out all HTML, added broken dev build script, layed out js.

---
+++ build/dev.sh
@@ -0,0 +1,4 @@
+mkdir ../bin
+mkdir ../bin/dev
+cp ../src/. ../bin/dev/.
+cp ../lib/jquery-2.2.3.js ../bin/dev/lib/jquery.js
--- src/j.js
+++ src/j.js
@@ -1,7 +1,13 @@
 "use strict";
+var apiClientIds = ["82f991a3e8e7949"];
+
+function getAlbum(var albumId){
+}
+
+function getApiClientId(){
+    return apiClientIds[Math.floor(Math.random() / (1 / apiClientIds.length))];
+}
+
+function setupPage(){}
 
 window.onload = setupPage();
-
-function setupPage(){
-    window.alert("Hello world, again.");
-}
