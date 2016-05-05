c8fa1bbe39db91ee14e0d4b7fa7136066acc11c2
Chilledpeperami <chilledpeperami@localhost.localdomain>
2016-05-05 18:01:31 +0200

    Developed JS to display image.

--- build/dev.sh
+++ build/dev.sh
@@ -1,4 +1,6 @@
 mkdir ../bin
 mkdir ../bin/dev
-cp ../src/. ../bin/dev/.
-cp ../lib/jquery-2.2.3.js ../bin/dev/lib/jquery.js
+mkdir ../bin/dev/lib
+cp -r ../src/. ../bin/dev
+cp ../lib/jquery-2.2.3.js ../bin/dev/lib/
+mv ../bin/dev/lib/jquery-2.2.3.js ../bin/dev/lib/jquery.js 
--- src/j.js
+++ src/j.js
@@ -1,13 +1,34 @@
 "use strict";
 var apiClientIds = ["82f991a3e8e7949"];
+var loadingImageURI = "./img/loading.gif";
 
-function getAlbum(var albumId){
-}
+function getAlbum(albumId){}
 
 function getApiClientId(){
     return apiClientIds[Math.floor(Math.random() / (1 / apiClientIds.length))];
 }
 
-function setupPage(){}
+function setupImage(imageURI){
+    document.getElementById("imageDisplay").src = loadingImageURI;
+    document.getElementById("imageDisplay").width = "100";
+    document.getElementById("imageDisplay").height = "100";
+    
+    var image = document.createElement("IMG");
+    image.setAttribute("src", imageURI);
+    image.onload = function (){
+        if(this.naturalWidth > this.naturalHeight){
+            document.getElementById("imageDisplay").width = document.getElementById("imageHolderWidth").offsetWidth;
+            document.getElementById("imageDisplay").style.height = "auto";
+        }else{
+            document.getElementById("imageDisplay").height = document.getElementById("imageHolderHeight").offsetHeight;
+            document.getElementById("imageDisplay").style.width = "auto";
+        }
+        document.getElementById("imageDisplay").src = this.src;
+    };
+}
+
+function setupPage(){
+    setupImage("./img/portrait.jpg");
+}
 
 window.onload = setupPage();
